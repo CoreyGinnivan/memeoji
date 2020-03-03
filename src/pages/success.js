aaa922cad9561e7b756c71f4cc883f24c67a6795
Corey <corey@ginnivan.net>
2020-03-03 19:01:27 +0800

    Cleaned up success page copy

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,13 +1,37 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import Jesus from "../images/memeojis/jesus.png"
+
 const Success = () => (
   <Layout>
     <SEO title="Memeoji Submission Successfukl" />
-    <h1>Thanks</h1>
-    <p>I'll let you know if your submission is accepted or not shortly.</p>
+    <Link to="/" className="mb-2" style={{ width: "fit-content" }}>
+      <span className="mr-2">←</span>Back Home
+    </Link>
+    <h1>
+      <img
+        src={Jesus}
+        alt="Jesus"
+        width="40"
+        className="inline-block mb-1 mr-2"
+      />
+      Submission successful!
+    </h1>
+    <p>
+      Check back shortly to see if your submission was successful, otherwise
+      chase me down on{" "}
+      <a
+        href="https://twitter.com/CoreyGinnivan"
+        target="_blank"
+        className="font-semibold text-black hover:underline"
+      >
+        Twitter ⤴
+      </a>
+    </p>
   </Layout>
 )
 
