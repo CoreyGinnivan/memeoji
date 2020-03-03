0372ee3b544f63b9f4153b7b5babc761b8b35dce
Corey <corey@ginnivan.net>
2020-03-03 18:32:10 +0800

    Form success page, reverted form

---
+++ src/pages/success.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Success = () => (
+  <Layout>
+    <SEO title="Memeoji Submission Successfukl" />
+    <h1>Thanks</h1>
+    <p>I'll let you know if your submission is accepted or not shortly.</p>
+  </Layout>
+)
+
+export default Success
